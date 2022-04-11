5323a3d327dc673b7cf0c6f9b10ec1b40ecb2798
omarfurkan <omarfurkan92@gmail.com>
2022-04-11 15:53:42 +0600

    good

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,6 +18,13 @@ const Header = () => {
                 <Link to='/products'>Products</Link>
                 <Link to='/orders'>Orders</Link>
                 <Link to='/register'>Register</Link>
+
+                {
+                    user && <>
+                        <Link to='/vip'>VIP</Link>
+                    </>
+                }
+
                 <span>{user?.displayName && user.displayName}</span>
                 {
                     user?.uid ?
